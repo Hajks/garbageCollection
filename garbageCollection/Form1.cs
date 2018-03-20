6b4a1f5a3c790438cfd3ce1e6d05e6d51a63c41a
Hajk <tunczorx@gmail.com>
2018-03-20 13:52:08 +0100

    Testing how garbage collector works

--- garbageCollection/Form1.cs
+++ garbageCollection/Form1.cs
@@ -12,9 +12,37 @@ namespace garbageCollection
 {
     public partial class Form1 : Form
     {
+        Elephant Lucinda;
+        Elephant Lloyd;
+
         public Form1()
         {
             InitializeComponent();
+            Lucinda = new Elephant() { Name = "Lucinda", EarSize = 55 };
+            Lloyd = new Elephant() { Name = "Lloyd", EarSize = 33 };
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Lloyd.WhoAmI();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Lucinda.WhoAmI();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+                Elephant Changer; //So if we want to swap references we have create empty object which gonna storage information for a while
+                Changer = Lucinda;
+                Lucinda = Lloyd;
+                Lloyd = Changer;
+                MessageBox.Show("Zamienione!");
+
+
         }
     }
 }
